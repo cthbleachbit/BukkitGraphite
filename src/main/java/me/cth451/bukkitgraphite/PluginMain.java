681a8a20e7ea909a5f6cb2c720f55ea3f5260676
Tianhao Chai <cth451@gmail.com>
2022-11-16 21:19:51 -0500

    Initial commit

---
+++ src/main/java/me/cth451/bukkitgraphite/PluginMain.java
@@ -0,0 +1,14 @@
+package me.cth451.bukkitgraphite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
